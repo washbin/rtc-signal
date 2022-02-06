8fcb234e37b577573cbcb84b3d5d19515bd095fb
washbin <76929116+washbin@users.noreply.github.com>
2022-02-06 17:29:59 +0545

    feat: initial implementation

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import {WebSocket, WebSocketServer} from 'ws';
+import {randomUUID} from 'crypto';
+
+const ws = new WebSocketServer({port: 9090});
+
+let clientsArray: Array<{connection: WebSocket; id: string}> = [];
+
+ws.on('listening', () => {
+  console.log('Server started listening in port 9090');
+});
+
+ws.on('connection', connection => {
+  const id = randomUUID();
+  clientsArray.push({connection, id});
+
+  connection.on('open', () => {
+    console.log('User connected', id);
+  });
+
+  connection.on('error', error => {
+    console.error('An Error has occured', error);
+  });
+
+  connection.on('message', message => {
+    clientsArray
+      .filter(client => client.id !== id)
+      .forEach(client =>
+        client.connection.send(
+          JSON.stringify({client: id, text: JSON.parse(message.toString())})
+        )
+      );
+    console.log('Message from', id);
+  });
+
+  connection.on('close', () => {
+    clientsArray = clientsArray.filter(client => client.id !== id);
+    clientsArray.forEach(client =>
+      client.connection.send(
+        JSON.stringify({client: id, text: 'I disconnected'})
+      )
+    );
+    console.log('User disconnected', id);
+  });
+});
